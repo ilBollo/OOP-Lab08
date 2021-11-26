e75c93fc253e7bd941b7723934c08c59b07b4796
Simone Bollini <simone.bollini@studio.unibo.it>
2021-11-26 16:08:15 +0100

    read and studied class codeanalysis

--- src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
+++ src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
@@ -3,6 +3,7 @@ package it.unibo.oop.lab.codeanalysis;
 import java.util.ArrayList;
 import java.util.Collection;
 
+
 /*
  * Checkstyle complains: the author and version Javadoc tags should not be used.
  */
@@ -10,12 +11,9 @@ import java.util.Collection;
  * This code triggers static code analyzers. You should use it to see how things
  * SHOULD NOT be done.
  *
- * @author Danilo Pianini
- * @version 1.2
- *
  */
-public class ThisIsHowThingsShouldNotBeDone {
-
+ final class ThisIsHowThingsShouldNotBeDone {
+ private static final int MAGIC = 43;
     /*
      * PMD complains:
      * 
@@ -43,13 +41,13 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * Missing Javadoc
      */
-    public static void main(String[] a) {
+    public static void main(final String[] a) {
         /*
          * PMD Complains:
          * 
          * this variable should be final!
          */
-        Collection<Object> c = new ArrayList<>();
+        final Collection<Object> c = new ArrayList<>();
         /*
          * PMD complains:
          * 
@@ -61,14 +59,15 @@ public class ThisIsHowThingsShouldNotBeDone {
          * 
          * What does it mean "43"? It's a magic number!
          */
-        for (int i = 0; i < 43; i++)
+        for (int i = 0; i < MAGIC; i++) {
             c.add(new Object());
+            }
         /*
          * FindBugs complains
          * 
          * Slow: use clear() instead!
          */
-        c.removeAll(c);
+        c.clear();
     }
 
 }
